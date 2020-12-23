ff26d8bb8c8ba42cff9e6b4d9b20dbda21786746
Paul Thorrington <3472481+PaulThor@users.noreply.github.com>
2020-12-23 13:37:25 +0000

    Updated the installment pages to match prototype

--- app/uk/gov/hmrc/vatdeferralnewpaymentschemefrontend/controllers/MonthsController.scala
+++ app/uk/gov/hmrc/vatdeferralnewpaymentschemefrontend/controllers/MonthsController.scala
@@ -9,7 +9,7 @@ import javax.inject.{Inject, Singleton}
 import play.api.mvc._
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.config.AppConfig
-import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.views.html.HowManyMonthsPage
+import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.views.html.{HowManyMonthsPage, MonthlyInstallmentsPage}
 import scala.concurrent.Future
 import play.api.i18n.I18nSupport
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -24,20 +24,46 @@ import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.model.JourneySession
 class MonthsController @Inject()(
   mcc: MessagesControllerComponents,
   auth: Auth,
+  monthlyInstallmentsPage: MonthlyInstallmentsPage,
   howManyMonthsPage: HowManyMonthsPage,
   sessionStore: SessionStore)
   (implicit val appConfig: AppConfig, val serviceConfig: ServicesConfig)
     extends FrontendController(mcc) with I18nSupport {
 
   val get: Action[AnyContent] = auth.authoriseWithJourneySession { implicit request => vrn => journeySession =>
-
     journeySession.outStandingAmount match {
-      case Some(outStandingAmount) => Future.successful(Ok(howManyMonthsPage(getMonths(outStandingAmount))))
+      case Some(outStandingAmount) => {
+        val monthlyAmount = (outStandingAmount / 11).setScale(2, RoundingMode.DOWN)
+        val remainder = outStandingAmount - (monthlyAmount * 11)
+        Future.successful(Ok(monthlyInstallmentsPage(monthlyAmount, remainder)))
+      }
       case _ => Future.successful(Redirect(routes.DeferredVatBillController.get()))
     }
   }
 
   val post: Action[AnyContent] = auth.authoriseWithJourneySession { implicit request => vrn => journeySession =>
+
+   val months = request.body.asFormUrlEncoded.flatMap(_.mapValues(_.last).get("11-months-to-pay"))
+
+    months.fold(Future.successful(BadRequest("error occured")))(month => {
+      if(month == "true") {
+        sessionStore.store[JourneySession](journeySession.id, "JourneySession", journeySession.copy(numberOfPaymentMonths = Some(11)))
+        Future.successful(Redirect(routes.WhenToPayController.get()))
+      }
+      else {
+        Future.successful(Redirect(routes.MonthsController.getInstallmentBreakdown()))
+      }
+    })
+  }
+
+  val getInstallmentBreakdown: Action[AnyContent] = auth.authoriseWithJourneySession { implicit request => vrn => journeySession =>
+    journeySession.outStandingAmount match {
+      case Some(outStandingAmount) => Future.successful(Ok(howManyMonthsPage(getMonths(outStandingAmount).reverse)))
+      case _ => Future.successful(Redirect(routes.DeferredVatBillController.get()))
+    }
+  }
+
+  val postInstallmentBreakdown: Action[AnyContent] = auth.authoriseWithJourneySession { implicit request => vrn => journeySession =>
 
       val months = request.body.asFormUrlEncoded.flatMap(_.mapValues(_.last).get("how-many-months"))
 
@@ -48,7 +74,7 @@ class MonthsController @Inject()(
   }
 
   def getMonths(amount: BigDecimal): Seq[Month] = {
-    (2 to 11).map {
+    (2 to 10).map {
       month => {
         val monthlyAmount = (amount / month).setScale(2, RoundingMode.DOWN)
         val remainder = amount - (monthlyAmount * month)
