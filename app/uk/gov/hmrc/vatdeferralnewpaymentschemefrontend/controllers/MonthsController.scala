98fd2517013338461f0eba4257ebaa8041979975
Paul Thorrington <3472481+PaulThor@users.noreply.github.com>
2021-01-05 07:31:38 +0000

    Added validation for the main journey

--- app/uk/gov/hmrc/vatdeferralnewpaymentschemefrontend/controllers/MonthsController.scala
+++ app/uk/gov/hmrc/vatdeferralnewpaymentschemefrontend/controllers/MonthsController.scala
@@ -1,24 +1,26 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2021 HM Revenue & Customs
  *
  */
 
 package uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.controllers
 
 import javax.inject.{Inject, Singleton}
+import play.api.data.Form
+import play.api.data.Forms.mapping
+import play.api.i18n.MessagesApi
 import play.api.mvc._
-import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
-import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.config.AppConfig
-import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.views.html.{HowManyMonthsPage, MonthlyInstallmentsPage}
-import scala.concurrent.Future
-import play.api.i18n.I18nSupport
-import scala.concurrent.ExecutionContext.Implicits.global
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.auth.Auth
-import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.viewmodel.Month
-import scala.math.BigDecimal.RoundingMode
-import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.services.SessionStore
+import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.config.AppConfig
 import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.model.JourneySession
+import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.services.SessionStore
+import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.viewmodel.Month
+import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.views.html.{HowManyMonthsPage, MonthlyInstallmentsPage}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.math.BigDecimal.RoundingMode
 
 @Singleton
 class MonthsController @Inject()(
@@ -28,49 +30,41 @@ class MonthsController @Inject()(
   howManyMonthsPage: HowManyMonthsPage,
   sessionStore: SessionStore)
   (implicit val appConfig: AppConfig, val serviceConfig: ServicesConfig)
-    extends FrontendController(mcc) with I18nSupport {
+    extends BaseController(mcc) {
 
   val get: Action[AnyContent] = auth.authoriseWithJourneySession { implicit request => vrn => journeySession =>
-    journeySession.outStandingAmount match {
-      case Some(outStandingAmount) => {
-        val monthlyAmount = (outStandingAmount / 11).setScale(2, RoundingMode.DOWN)
-        val remainder = outStandingAmount - (monthlyAmount * 11)
-        Future.successful(Ok(monthlyInstallmentsPage(monthlyAmount, remainder)))
-      }
-      case _ => Future.successful(Redirect(routes.DeferredVatBillController.get()))
-    }
+    displayInstalmentsPage(journeySession, instalmentsForm)
   }
 
   val post: Action[AnyContent] = auth.authoriseWithJourneySession { implicit request => vrn => journeySession =>
 
-   val months = request.body.asFormUrlEncoded.flatMap(_.mapValues(_.last).get("11-months-to-pay"))
-
-    months.fold(Future.successful(BadRequest("error occured")))(month => {
-      if(month == "true") {
-        sessionStore.store[JourneySession](journeySession.id, "JourneySession", journeySession.copy(numberOfPaymentMonths = Some(11)))
-        Future.successful(Redirect(routes.WhenToPayController.get()))
+    instalmentsForm.bindFromRequest().fold(
+      errors => displayInstalmentsPage(journeySession, errors),
+      formValue => {
+          if(formValue.value == "true") {
+            sessionStore.store[JourneySession](journeySession.id, "JourneySession", journeySession.copy(numberOfPaymentMonths = Some(11)))
+            Future.successful(Redirect(routes.WhenToPayController.get()))
+          }
+          else {
+            Future.successful(Redirect(routes.MonthsController.getInstallmentBreakdown()))
+          }
       }
-      else {
-        Future.successful(Redirect(routes.MonthsController.getInstallmentBreakdown()))
-      }
-    })
+    )
   }
 
-  val getInstallmentBreakdown: Action[AnyContent] = auth.authoriseWithJourneySession { implicit request => vrn => journeySession =>
-    journeySession.outStandingAmount match {
-      case Some(outStandingAmount) => Future.successful(Ok(howManyMonthsPage(getMonths(outStandingAmount).reverse)))
-      case _ => Future.successful(Redirect(routes.DeferredVatBillController.get()))
-    }
+  val getInstallmentBreakdown: Action[AnyContent] = auth.authoriseWithJourneySession { implicit request =>vrn =>journeySession =>
+    displayMonthsSelectionPage(journeySession, monthForm)
   }
 
   val postInstallmentBreakdown: Action[AnyContent] = auth.authoriseWithJourneySession { implicit request => vrn => journeySession =>
 
-      val months = request.body.asFormUrlEncoded.flatMap(_.mapValues(_.last).get("how-many-months"))
-
-      months.fold(Future.successful(BadRequest("error occured")))(month => {
-        sessionStore.store[JourneySession](journeySession.id, "JourneySession", journeySession.copy(numberOfPaymentMonths = Some(month.toInt)))
-        Future.successful(Redirect(routes.WhenToPayController.get()))
-      })
+    monthForm.bindFromRequest().fold(
+      errors => displayMonthsSelectionPage(journeySession, errors),
+      form => {
+          sessionStore.store[JourneySession](journeySession.id, "JourneySession", journeySession.copy(numberOfPaymentMonths = Some(form.value.toInt)))
+          Future.successful(Redirect(routes.WhenToPayController.get()))
+      }
+    )
   }
 
   def getMonths(amount: BigDecimal): Seq[Month] = {
@@ -82,4 +76,35 @@ class MonthsController @Inject()(
       }
     }
   }
+
+  def displayInstalmentsPage(journeySession: JourneySession, form: Form[FormValues])(implicit request: Request[_], messages: MessagesApi, appConfig: AppConfig) = {
+    journeySession.outStandingAmount match {
+      case Some(outStandingAmount) => {
+        val monthlyAmount = (outStandingAmount / 11).setScale(2, RoundingMode.DOWN)
+        val remainder = outStandingAmount - (monthlyAmount * 11)
+
+        if (form.hasErrors) Future.successful(BadRequest(monthlyInstallmentsPage(monthlyAmount, remainder, form)))
+        else Future.successful(Ok(monthlyInstallmentsPage(monthlyAmount, remainder, form)))
+      }
+      case _ => Future.successful(Redirect(routes.DeferredVatBillController.get()))
+    }
+  }
+
+  def displayMonthsSelectionPage(journeySession: JourneySession, form: Form[FormValues])(implicit request: Request[_], messages: MessagesApi, appConfig: AppConfig) = {
+    journeySession.outStandingAmount match {
+      case Some(outStandingAmount) => {
+        if (form.hasErrors) Future.successful(BadRequest(howManyMonthsPage(getMonths(outStandingAmount).reverse, form)))
+        else Future.successful(Ok(howManyMonthsPage(getMonths(outStandingAmount).reverse, form)))
+      }
+      case _ => Future.successful(Redirect(routes.DeferredVatBillController.get()))
+    }
+  }
+
+  val instalmentsForm: Form[FormValues] = Form(
+    mapping("11-months-to-pay" -> mandatory("11monthstopay"))(FormValues.apply)(FormValues.unapply))
+
+  val monthForm: Form[FormValues] = Form(
+    mapping("how-many-months" -> mandatory("how-many-months"))(FormValues.apply)(FormValues.unapply))
+
+  case class FormValues(value: String)
 }
--- app/uk/gov/hmrc/vatdeferralnewpaymentschemefrontend/controllers/WhenToPayController.scala
+++ app/uk/gov/hmrc/vatdeferralnewpaymentschemefrontend/controllers/WhenToPayController.scala
@@ -1,23 +1,21 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2021 HM Revenue & Customs
  *
  */
 
 package uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.controllers
 
 import javax.inject.{Inject, Singleton}
-import play.api.i18n.{I18nSupport, Lang, MessagesApi}
+import play.api.data.Form
+import play.api.data.Forms.mapping
 import play.api.mvc._
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
-import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.auth.Auth
 import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.config.AppConfig
 import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.connectors.BavfConnector
-import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.model.Bavf.{InitRequestMessages, InitResponse}
 import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.model.JourneySession
 import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.services.SessionStore
 import uk.gov.hmrc.vatdeferralnewpaymentschemefrontend.views.html.WhenToPayPage
-import play.api.libs.json.Json
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -30,26 +28,31 @@ class WhenToPayController @Inject()(
   connector: BavfConnector,
   sessionStore: SessionStore)
     (implicit val appConfig: AppConfig, val serviceConfig: ServicesConfig)
-    extends FrontendController(mcc) with I18nSupport {
+    extends BaseController(mcc) {
 
   val get: Action[AnyContent] = auth.authoriseWithJourneySession { implicit request =>
     vrn =>
       journeySession =>
 
         journeySession.outStandingAmount match {
-          case Some(_) => Future.successful(Ok(whenToPagePage()))
+          case Some(_) => Future.successful(Ok(whenToPagePage(frm)))
           case _ => Future.successful(Redirect(routes.DeferredVatBillController.get()))
         }
   }
 
-  val post: Action[AnyContent] = auth.authoriseWithJourneySession { implicit request =>
-    vrn =>
-      journeySession =>
+  val post: Action[AnyContent] = auth.authoriseWithJourneySession { implicit request => vrn => journeySession =>
 
-        val days = request.body.asFormUrlEncoded.map(a => a.mapValues(_.last)).flatMap(b => b.get("day"))
-        days.fold(Future.successful(BadRequest("error occured")))(day => {
-          sessionStore.store[JourneySession](journeySession.id, "JourneySession", journeySession.copy(dayOfPayment = Some(if (day.isEmpty) 28 else day.toInt)))
-          Future.successful(Redirect(routes.PaymentPlanController.get()))
-        })
+        frm.bindFromRequest().fold(
+          errors => Future.successful(BadRequest(whenToPagePage(errors))),
+          form => {
+            sessionStore.store[JourneySession](journeySession.id, "JourneySession", journeySession.copy(dayOfPayment = Some(form.value.toInt)))
+            Future.successful(Redirect(routes.PaymentPlanController.get()))
+          }
+        )
   }
+
+  val frm: Form[FormValues] = Form(
+    mapping("day" -> mandatoryAndValid("day", "^(2[0-8]|[1][0-9]|[1-9])$"))(FormValues.apply)(FormValues.unapply))
+
+  case class FormValues(value: String)
 }
